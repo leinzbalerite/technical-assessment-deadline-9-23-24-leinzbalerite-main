8930d1fb24a3a2517324072722320ecf3cb8880b
leinzbalerite <leinzbalerite@gmail.com>
2024-09-21 22:09:41 -0500

    Started function to calculate true shooting percentage

--- technical-assessment-deadline-9-23-24-leinzbalerite-main/game_finder.py
+++ technical-assessment-deadline-9-23-24-leinzbalerite-main/game_finder.py
@@ -6,7 +6,17 @@ Given the following inputs:
 
 Implement find_qualified_games to return a list of unique qualified gameIDs in which at least <player_count> players have a True Shooting percentage >= <true_shooting_cutoff>, ordered from most to least recent game.
 """
+def calculate_true_shooting_percentage(player:dict) -> float:
+	fieldGoal2_attempted = player['fieldGoal2Attempted']
+	fieldGoal2_made = player ['fieldGoal2Made']
+	fieldGoal3_attempted = player ['fieldGoal3Attempted']
+	fieldGoal3_made =  player['fieldGoal3Made']
+	freeThrow_attempted = player['freeThrowAttempted']
+	freeThrow_made = player['freeThrowMade']
+
+
+	points = 
 
 def find_qualified_games(game_data: list[dict], true_shooting_cutoff: int, player_count: int) -> list[int]:
-	# Replace the line below with your code
+	
 	pass
