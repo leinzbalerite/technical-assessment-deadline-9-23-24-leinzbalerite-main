ea50e6dbbc93f8726de0dcaccd9673ca05b64d6e
leinzbalerite <leinzbalerite@gmail.com>
2024-09-21 22:08:56 -0500

    first commit

---
+++ technical-assessment-deadline-9-23-24-leinzbalerite-main/README.md
@@ -0,0 +1,19 @@
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/mxYpjcnI)
+# OKC Thunder Technical Assessment
+
+### Internship Program Disclosures
+
+* You must be eligible to work in the United States to qualify for this internship.
+  
+* The pay for this internship is the greater of your local minimum wage and $13/hour.
+
+* This application is for the purposes of an internship taking place in the Spring, Summer, or Fall of 2025.
+
+### Task
+
+Your task is to implement find_qualified_games in game_finder.py. Detailed instructions for this technical assessment can be found in game_finder.py. Some sample test cases are provided in the `tests` folder, but additional ones may be used to evaluate your code.
+
+
+# Questions?
+
+Email datasolutions@okcthunder.com
---
+++ technical-assessment-deadline-9-23-24-leinzbalerite-main/game_finder.py
@@ -0,0 +1,12 @@
+"""
+Given the following inputs:
+- <game_data> is a list of dictionaries, with each dictionary representing a player's shot attempts in a game. The list can be empty, but any dictionary in the list will include the following keys: gameID, playerID, gameDate, fieldGoal2Attempted, fieldGoal2Made, fieldGoal3Attempted, fieldGoal3Made, freeThrowAttempted, freeThrowMade. All values in this dictionary are ints, except for gameDate which is of type str in the format 'MM/DD/YYYY'
+- <true_shooting_cutoff> is the minimum True Shooting percentage value for a player to qualify in a game. It will be an int value >= 0.
+- <player_count> is the number of players that need to meet the <true_shooting_cutoff> in order for a gameID to qualify. It will be an int value >= 0.
+
+Implement find_qualified_games to return a list of unique qualified gameIDs in which at least <player_count> players have a True Shooting percentage >= <true_shooting_cutoff>, ordered from most to least recent game.
+"""
+
+def find_qualified_games(game_data: list[dict], true_shooting_cutoff: int, player_count: int) -> list[int]:
+	# Replace the line below with your code
+	pass
